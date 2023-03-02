cf5dfa97b3ce1c3a44029d19169f5d5473258f20
Saad Siddiq <siddiq.saad@gmail.com>
2023-03-02 15:44:55 -0700

    moved create crosshair and fpsLimiter to helper

--- dist/helpers.js
+++ dist/helpers.js
@@ -3,8 +3,15 @@ export const stats = new Stats();
 stats.dom.style.right = '0px';
 stats.dom.style.left = '';
 document.body.appendChild(stats.dom);
-export const getRandom = (min, max) => {
-    return Math.random() * (max - min) + min;
+export const getRandomBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+export const createCenterCrosshair = () => {
+    const crosshair = new Image();
+    crosshair.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAZQTFRFAAAA////pdmf3QAAAAJ0Uk5TAP9bkSK1AAAAE0lEQVR4nGNgbGDARP//gxA2KQAFtgsLandQLgAAAABJRU5ErkJggg==';
+    return (ctx, canvas) => {
+        ctx.drawImage(crosshair, (canvas.width / 2) - (crosshair.width / 2), (canvas.height / 2) - (crosshair.height / 2));
+    };
 };
 let now = 0;
 let then = 0;
@@ -14,6 +21,19 @@ export const getDelta = () => {
     delta = (now - then) / 1000;
     then = now;
     if (delta > 0.1)
-        return 0.1;
+        return 0.01;
     return delta;
+};
+export const createFPSLimiter = (fps) => {
+    let fpsInterval = 1000 / fps;
+    let before = performance.now();
+    let elapsed = 0;
+    return (now) => {
+        elapsed = now - before;
+        if (elapsed > fpsInterval) {
+            before = now - (elapsed % fpsInterval);
+            return true;
+        }
+        return false;
+    };
 };
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,14 +1,29 @@
 // @ts-ignore
 import Stats from '../lib/stats.js';
 
+
+// Add an FPS tracker to top right corner
 export const stats = new Stats();
 stats.dom.style.right = '0px';
 stats.dom.style.left = '';
 document.body.appendChild(stats.dom);
 
-export const getRandom = (min: number, max: number): number => {
-  return Math.random() * (max - min) + min;
+
+// Get random number between two numbers (min and max inclusive)
+export const getRandomBetween = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+// Returns function to draw small '+' in center of canvas
+export const createCenterCrosshair = () => {
+  const crosshair = new Image();
+  crosshair.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAZQTFRFAAAA////pdmf3QAAAAJ0Uk5TAP9bkSK1AAAAE0lEQVR4nGNgbGDARP//gxA2KQAFtgsLandQLgAAAABJRU5ErkJggg==';
+
+  return (ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) => {
+    ctx.drawImage(crosshair, (canvas.width / 2) - (crosshair.width / 2), (canvas.height / 2) - (crosshair.height / 2));
+  }
+}
+
 
 let now = 0;
 let then = 0;
@@ -18,7 +33,24 @@ export const getDelta = (): number => {
   delta = (now - then) / 1000;
   then = now;
 
-  // Limit delta for when coming back to browser tab after a while
-  if (delta > 0.1) return 0.1;
+  if (delta > 0.1) return 0.01;  // Limit delta for when coming back to browser tab after a while
+
   return delta;
+}
+
+export const createFPSLimiter = (fps: number) => {
+  let fpsInterval = 1000 / fps;
+  let before = performance.now();
+  let elapsed = 0;
+
+  return (now: DOMHighResTimeStamp): boolean => {
+    elapsed = now - before;
+  
+    if (elapsed > fpsInterval) {
+      before = now - (elapsed % fpsInterval);
+      return true;
+    }
+  
+    return false;
+  }
 }
