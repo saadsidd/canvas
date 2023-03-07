9daeaf0157beeea1d575975a070097b61f2f6881
Saad Siddiq <siddiq.saad@gmail.com>
2023-03-07 14:14:28 -0700

    removed crosshair.png, 60 fps, no delta

--- dist/main2.js
+++ dist/main2.js
@@ -1,5 +1,6 @@
-import { stats, getDelta, createCenterCrosshair, createFPSLimiter } from "./helpers.js";
-console.log('main2');
+import { createCenterCrosshair, createFPSLimiter } from "./helpers.js";
+const trailsCheckbox = document.getElementById('trails-checkbox');
+const clearButton = document.getElementById('clear-button');
 const canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -7,8 +8,7 @@ const ctx = canvas.getContext('2d');
 const particlesArray = [];
 const fpsIntervalElapsed = createFPSLimiter(60);
 const drawCenterCrosshair = createCenterCrosshair();
-let dt = 0;
-let hue = 0;
+let showTrails = false;
 const mouse = {
     x: 0,
     y: 0,
@@ -18,45 +18,46 @@ const mouse = {
         this.y = newY;
     }
 };
+trailsCheckbox.addEventListener('change', function () { showTrails = this.checked; });
+clearButton.addEventListener('click', function () { particlesArray.length = 0; });
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    particlesArray.length = 0;
 });
 canvas.addEventListener('mousedown', () => { mouse.clicked = true; });
 canvas.addEventListener('mouseup', () => { mouse.clicked = false; });
 canvas.addEventListener('mouseleave', () => { mouse.clicked = false; });
 canvas.addEventListener('mousemove', (event) => {
     mouse.update(event.offsetX, event.offsetY);
-    if (mouse.clicked && particlesArray.length < 200) {
+    if (mouse.clicked && particlesArray.length < 1000) {
         particlesArray.push(new Particle());
     }
 });
 canvas.addEventListener('click', (event) => {
-    if (particlesArray.length < 200) {
+    if (particlesArray.length < 1000) {
         mouse.update(event.offsetX, event.offsetY);
         particlesArray.push(new Particle());
     }
 });
 class Particle {
     constructor() {
-        const speed = 60;
         this.x = mouse.x;
         this.y = mouse.y;
-        this.radius = 6;
-        this.speedX = 0;
-        this.speedY = 0;
-        this.color = `hsl(${hue}, 100%, 50%)`;
+        this.radius = 4;
+        this.color = `hsl(${Particle.hue}, 100%, 50%)`;
         this.isDone = false;
+        Particle.hue += 1;
         this.targetX = canvas.width / 2;
         this.targetY = canvas.height / 2;
         this.i = 0;
     }
     move() {
-        this.i++;
+        this.i += 1;
         const x1 = this.x - this.targetX;
         const y1 = this.y - this.targetY;
-        const x2 = x1 * Math.cos(this.i * 0.001) - y1 * Math.sin(this.i * 0.001);
-        const y2 = x1 * Math.sin(this.i * 0.001) + y1 * Math.cos(this.i * 0.001);
+        const x2 = x1 * Math.cos(this.i * 0.01) - y1 * Math.sin(this.i * 0.01);
+        const y2 = x1 * Math.sin(this.i * 0.01) + y1 * Math.cos(this.i * 0.01);
         this.x = x2 + this.targetX;
         this.y = y2 + this.targetY;
     }
@@ -66,32 +67,29 @@ class Particle {
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
     }
-    shrink() {
-        if (this.radius > 0.2)
-            this.radius -= 2 * dt;
-    }
     update() {
         if (this.radius <= 0.3)
             this.isDone = true;
-        this.shrink();
         this.move();
         this.draw();
     }
 }
+Particle.hue = Math.floor(Math.random() * 360);
 const render = (now) => {
     if (fpsIntervalElapsed(now)) {
-        stats.begin();
-        dt = getDelta();
-        hue += 200 * dt;
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        if (showTrails) {
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+        else {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
         drawCenterCrosshair(ctx, canvas);
         particlesArray.forEach((particle, index) => {
             particle.update();
             if (particle.isDone)
                 particlesArray.splice(index, 1);
         });
-        stats.end();
     }
     requestAnimationFrame(render);
 };
--- src/main2.ts
+++ src/main2.ts
@@ -1,9 +1,10 @@
-// spinning white/color spirals
+// Spinning Particles Around Center Project
 
-import { stats, getDelta, createCenterCrosshair, createFPSLimiter } from "./helpers.js";
+import { createCenterCrosshair, createFPSLimiter } from "./helpers.js";
 import Mouse from "./interfaces/mouse.js";
-console.log('main2');
 
+const trailsCheckbox = document.getElementById('trails-checkbox') as HTMLInputElement;
+const clearButton = document.getElementById('clear-button') as HTMLButtonElement;
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -13,12 +14,11 @@ const particlesArray: Particle[] = [];
 const fpsIntervalElapsed = createFPSLimiter(60);
 const drawCenterCrosshair = createCenterCrosshair();
 
-let dt = 0;
-let hue = 0;
+let showTrails = false;
 
 const mouse: Mouse = {
   x: 0,
-  y: 0, 
+  y: 0,
   clicked: false,
   update(newX, newY) {
     this.x = newX;
@@ -26,37 +26,27 @@ const mouse: Mouse = {
   }
 }
 
+trailsCheckbox.addEventListener('change', function(): void { showTrails = this.checked });
+clearButton.addEventListener('click', function(): void { particlesArray.length = 0 });
+
 window.addEventListener('resize', (): void => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  particlesArray.length = 0;
 });
 canvas.addEventListener('mousedown', (): void => { mouse.clicked = true });
 canvas.addEventListener('mouseup', (): void =>  { mouse.clicked = false });
 canvas.addEventListener('mouseleave', (): void => { mouse.clicked = false });
 
-// canvas.addEventListener('mousemove', (event: MouseEvent): void => {
-//   if (mouse.clicked) {
-//     mouse.update(event.offsetX, event.offsetY);
-//     for (let i = 0; i < 5; i++) {
-//       particlesArray.push(new Particle());
-//     }
-//   }
-
-// });
-
 canvas.addEventListener('mousemove', (event: MouseEvent): void => {
   mouse.update(event.offsetX, event.offsetY);
-  if (mouse.clicked && particlesArray.length < 200) {
+  if (mouse.clicked && particlesArray.length < 1000) {
     particlesArray.push(new Particle());
   }
 });
 
-// canvas.addEventListener('mousemove', (event: MouseEvent): void => {
-//   mouse.update(event.offsetX, event.offsetY);
-// });
-
 canvas.addEventListener('click', (event: MouseEvent): void => {
-  if (particlesArray.length < 200) {
+  if (particlesArray.length < 1000) {
     mouse.update(event.offsetX, event.offsetY);
     particlesArray.push(new Particle());
   }
@@ -66,8 +56,6 @@ class Particle {
   x: number;
   y: number;
   radius: number;
-  speedX: number;
-  speedY: number;
   color: string;
   isDone: boolean;
 
@@ -75,64 +63,35 @@ class Particle {
   targetY: number;
   i: number;
 
-  constructor() {
-    const speed = 60;
+  static hue = Math.floor(Math.random() * 360);
 
+  constructor() {
     this.x = mouse.x;
     this.y = mouse.y;
-    this.radius = 6;
-    // this.speedX = Math.random() < 0.5 ? -speed : speed;
-    // this.speedY = Math.random() < 0.5 ? -speed : speed;
-    this.speedX = 0;
-    this.speedY = 0;
-    // this.color = `hsl(${getRandom(0, 360)}, 100%, 50%)`;
-    // this.color = 'white';
-    this.color = `hsl(${hue}, 100%, 50%)`;
+    this.radius = 4;
+
+    this.color = `hsl(${Particle.hue}, 100%, 50%)`;
     this.isDone = false;
+
+    Particle.hue += 1;
 
     this.targetX = canvas.width / 2;
     this.targetY = canvas.height / 2;
     this.i = 0;
-
-    // this.x = this.targetX;
-    // this.y = this.targetY;
   }
 
   move(): void {
 
-    this.i++;
-
+    this.i += 1;
     const x1 = this.x - this.targetX;
     const y1 = this.y - this.targetY;
 
-    const x2 = x1 * Math.cos(this.i * 0.001) - y1 * Math.sin(this.i * 0.001);
-    const y2 = x1 * Math.sin(this.i * 0.001) + y1 * Math.cos(this.i * 0.001);
+    const x2 = x1 * Math.cos(this.i * 0.01) - y1 * Math.sin(this.i * 0.01);
+    const y2 = x1 * Math.sin(this.i * 0.01) + y1 * Math.cos(this.i * 0.01);
 
     this.x = x2 + this.targetX;
     this.y = y2 + this.targetY;
-    // const dx = this.targetX - this.x;
-    // const dy = this.targetY - this.y;
-    // const distance = Math.sqrt(dx * dx + dy * dy);
-    // if (distance > 100) {
-    //   // console.log(distance);
-    //   this.speedX = dx;
-    //   this.speedY = dy;
-    // } else {
-    // this.i++;
-    // this.speedX = 300 * Math.cos(this.i * 0.01);
-    // this.speedY = 300 * Math.sin(this.i * 0.01);
-    // }
 
-    // // this.i++;
-    // this.i++;
-    // this.speedX = 300 * Math.cos(this.i * 0.01);
-    // this.speedY = 300 * Math.sin(this.i * 0.01);
-
-    // this.speedX = this.x * Math.cos(this.i * 0.01) - this.y * Math.sin(this.i * 0.01);
-    // this.speedY = this.y * Math.cos(this.i * 0.01) + this.x * Math.sin(this.i * 0.01);
-
-    // this.x += this.speedX * dt;
-    // this.y += this.speedY * dt;
   }
 
   draw(): void {
@@ -142,13 +101,8 @@ class Particle {
     ctx.fill();
   }
 
-  shrink(): void {
-    if (this.radius > 0.2) this.radius -= 2 * dt;
-  }
-
   update(): void {
     if (this.radius <= 0.3) this.isDone = true;
-    this.shrink();
     this.move();
     this.draw();
   }
@@ -157,15 +111,14 @@ class Particle {
 const render = (now: DOMHighResTimeStamp) => {
 
   if (fpsIntervalElapsed(now)) {
-    stats.begin();
-    
-    dt = getDelta();
-    hue += 200 * dt;
-  
+
     // Clear canvas
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    if (showTrails) {
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    } else {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
   
     drawCenterCrosshair(ctx, canvas);
   
@@ -173,8 +126,7 @@ const render = (now: DOMHighResTimeStamp) => {
       particle.update();
       if (particle.isDone) particlesArray.splice(index, 1);
     });
-  
-    stats.end();
+    
   }
 
   requestAnimationFrame(render);
